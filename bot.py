723b2b918921de474fe4d64aab34ac69da3e7e4a
MaaaaaaaJiaHui <60855244+MaaaaaaaJiaHui@users.noreply.github.com>
2020-07-13 14:43:32 +0800

    add basic bot

---
+++ bot.py
@@ -0,0 +1,74 @@
+from ananas import PineappleBot, hourly, schedule, reply, html_strip_tags, daily, interval
+import logging
+import bot_datacenter
+from bs4 import BeautifulSoup
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+class HelloBot(PineappleBot):
+
+    def start(self):
+        """
+        Do somethin when start the bot.
+        """
+        logging.info('Start hello bot ...')
+
+    @reply
+    def test(self, mention, user):
+        """
+        Reply for private message.
+        """
+        logging.info('it aaa')
+        print(mention)
+        print(user)
+        logging.info("Got a mention from {} at {}".format(user["username"], mention["created_at"]))
+
+        # if the notification is direct
+        # (private message)
+        if mention["visibility"] == "direct":
+
+            # get sender
+
+
+            # get message
+            soup = BeautifulSoup(mention["content"], "html.parser")
+            message = soup.find('p').get_text().strip().split(' ')[-1]
+            # print('read message {}'.format(message))
+
+            # check if the sender has saved messages
+
+            # make response based on the message
+            if message == 'hello':
+                welcome_message = bot_datacenter.show_introduction(self.mastodon)
+                welcome_full_message = welcome_message + "@{}".format(user["acct"])
+                self.mastodon.status_post(status=welcome_full_message, visibility="direct")
+
+            elif message == 'school info':
+                bot_datacenter.school_info()
+            elif message == 'school official website':
+                bot_datacenter.school_official_website()
+            elif message == 'school_info':
+                bot_datacenter.school_info()
+            elif message == 'school_official_website':
+                bot_datacenter.school_official_website()
+            elif message == 'school_contact_info':
+                bot_datacenter.school_contact_info()
+            elif message == 'school_newbee_info':
+                bot_datacenter.school_newbee_info()
+            elif message == 'school_f_and_q':
+                bot_datacenter.school_f_and_q()
+            elif message == 'class_info':
+                bot_datacenter.class_info()
+            elif message == 'meeting_book':
+                bot_datacenter.meeting_book()
+            elif message == 'teach_feedback':
+                bot_datacenter.teach_feedback()
+            elif message == 'question_history':
+                bot_datacenter.question_history()
+            elif message == 'other_questions':
+                bot_datacenter.other_questions()
+            else:
+                # if don't understand what he/she say, just display function introduction message
+                bot_datacenter.show_introduction()
+
---
+++ bot_datacenter.py
@@ -0,0 +1,83 @@
+
+def show_introduction(mastodon):
+    """
+    Introduce the bot functions.
+    """
+    message = "Hello, I'm School Bot, nice to meet you! What do want to know?"
+    print(message)
+    return message
+
+def school_info():
+    """
+    Display school info, let students choose options
+    """
+    message = "What do you want to konw about school?"
+    print(message)
+
+def school_official_website():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+def school_contact_info():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+def school_newbee_info():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+def school_f_and_q():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+
+
+
+
+
+def class_info():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+def meeting_book():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+def teach_feedback():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+def question_history():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
+
+def other_questions():
+    """
+    Display info, let students choose options
+    """
+    message = "This function is developing"
+    print(message)
