5618c9e696e1b326216fa89edd0468c264f04559
MaaaaaaaJiaHui <18682301291@163.com>
2020-08-09 09:12:16 +0800

    add exam, question, teacher and query history modules

--- mastodonrobot/school_info/urls.py
+++ mastodonrobot/school_info/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('course_post', views.course_post, name='course_post'),
 
     # exam
-    # path('exam/<int:exam_id>', views.exam_delete_or_update, name='exam_delete_or_update'),
-    # path('exam', views.exam_index, name='exam_index'),
-    # path('exam_post', views.exam_post, name='exam_post'),
+    path('exam/<int:exam_id>', views.exam_delete_or_update, name='exam_delete_or_update'),
+    path('exam', views.exam_index, name='exam_index'),
+    path('exam_post', views.exam_post, name='exam_post'),
 
     # # assignment
     # path('assignment/<int:assignment_id>', views.assignment_delete_or_update, name='assignment_delete_or_update'),
@@ -25,17 +25,17 @@ urlpatterns = [
     # path('assignment_post', views.assignment_post, name='assignment_post'),
 
     # # question
-    # path('question/<int:question_id>', views.question_delete_or_update, name='question_delete_or_update'),
-    # path('question', views.question_index, name='question_index'),
+    path('question/<int:question_id>', views.question_delete_or_update, name='question_delete_or_update'),
+    path('question', views.question_index, name='question_index'),
     # path('question_post', views.question_post, name='question_post'),
 
-    # # query history
-    # path('query_history/<int:query_history_id>', views.query_history_delete_or_update, name='query_history_delete_or_update'),
-    # path('query_history', views.query_history_index, name='query_history_index'),
+    # query history
+    path('query_history/<int:query_history_id>', views.query_history_delete_or_update, name='query_history_delete_or_update'),
+    path('query_history', views.query_history_index, name='query_history_index'),
     # path('query_history_post', views.query_history_post, name='query_history_post'),
 
-    # # teacher
-    # path('teacher/<int:teacher_id>', views.teacher_delete_or_update, name='teacher_delete_or_update'),
-    # path('teacher', views.teacher_index, name='teacher_index'),
-    # path('teacher_post', views.teacher_post, name='teacher_post'),
+    # teacher
+    path('teacher/<int:teacher_id>', views.teacher_delete_or_update, name='teacher_delete_or_update'),
+    path('teacher', views.teacher_index, name='teacher_index'),
+    path('teacher_post', views.teacher_post, name='teacher_post'),
 ]
--- mastodonrobot/school_info/views.py
+++ mastodonrobot/school_info/views.py
@@ -229,3 +229,309 @@ def course_delete_or_update(request, course_id):
 
     result = {'status':'success', 'message':message, 'data':{'table':table}}
     return HttpResponse(json.dumps(result))
+
+
+@require_http_methods(["GET"])
+def exam_index(request):
+    """
+    Display exam list page
+    """
+    exams = Exam.objects.all().filter(deleted_at__isnull=True)
+
+    content_dict = {}
+    content_dict['exams'] = exams
+    content_dict['current_nav_id'] = 'nav-exam'
+    return render(request, 'exam/index.html', content_dict)
+
+@require_http_methods(["POST"])
+def exam_post(request):
+
+    # get data from inputs
+    name = request.POST.get('name', None)
+    description = request.POST.get('description', None)
+    exam_at = request.POST.get('exam_at', None)
+    exam_type = int(request.POST.get('exam_type', None)) # 0: real; 1: online
+    location = request.POST.get('location', None)
+    url = request.POST.get('url', None)
+
+    # check if data illegal
+    if name is None or name == '':
+        result = {'status':'error', 'message':'Illegal name!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    if description is None or description == '':
+        result = {'status':'error', 'message':'Illegal description!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    if exam_at is None or exam_at == '':
+        result = {'status':'error', 'message':'Please input correct exam time!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    if exam_type is None:
+        result = {'status':'error', 'message':'Illegal exam type!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    if exam_type == 0 and (location is None or location == ''):
+        result = {'status':'error', 'message':'Please input location!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    if url is None:
+        url = ''
+
+    # create new exam
+    exam = Exam.objects.create(name=name, description=description, exam_at=exam_at, exam_type=exam_type, location=location, url=url)
+
+    # transfer exams objects to table data
+    exams = Exam.objects.all().filter(deleted_at__isnull=True)
+    table = []
+    for exam in exams:
+        table.append([
+            exam.name,
+            exam.description,
+            exam.exam_at,
+            exam.exam_type,
+            exam.location,
+            exam.url,
+            '<button type="button" class="btn btn-default">Edit</button> <button type="button" class="btn btn-danger" onclick="delete_row({})">Delete</button>'.format(exam.id)
+        ])
+
+    result = {'status':'success', 'message':'Create success!', 'data':{'table':table}}
+    return HttpResponse(json.dumps(result))
+
+@require_http_methods(["DELETE", "PUT"])
+def exam_delete_or_update(request, exam_id):
+    # get object
+    exam = Exam.objects.get(id=exam_id, deleted_at__isnull=True)
+
+    # check if data illegal
+    if exam is None:
+        result = {'status':'error', 'message':'This exam not exists!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    
+    if request.method == 'DELETE':
+        # delete it
+        exam.delete()
+        message = 'Delete success!'
+    elif request.method == 'PUT':
+        from django.http import QueryDict
+        put = QueryDict(request.body)
+
+        # get data from inputs
+        name = put.get('name', None)
+        description = put.get('description', None)
+        exam_at = put.get('exam_at', None)
+        exam_type = int(put.get('exam_type', None)) # 0: real; 1: online
+        location = put.get('location', None)
+        url = put.get('url', None)
+
+        # check if data illegal
+        if name is None or name == '':
+            result = {'status':'error', 'message':'Illegal name!', 'data':{}}
+            return HttpResponse(json.dumps(result))
+        if description is None or description == '':
+            result = {'status':'error', 'message':'Illegal description!', 'data':{}}
+            return HttpResponse(json.dumps(result))
+        if exam_at is None or exam_at == '':
+            result = {'status':'error', 'message':'Please input correct exam time!', 'data':{}}
+            return HttpResponse(json.dumps(result))
+        if exam_type is None:
+            result = {'status':'error', 'message':'Illegal exam type!', 'data':{}}
+            return HttpResponse(json.dumps(result))
+        if exam_type == 0 and (location is None or location == ''):
+            result = {'status':'error', 'message':'Please input location!', 'data':{}}
+            return HttpResponse(json.dumps(result))
+        if url is None:
+            url = ''
+        
+        # update data
+        exam.name = name
+        exam.description = description
+        exam.exam_at = exam_at
+        exam.exam_type = exam_type
+        exam.exam_type = exam_type
+        exam.url = url
+        exam.save()
+
+        message = 'Update success!'
+
+    exams = Exam.objects.all().filter(deleted_at__isnull=True)
+    table = []
+    for exam in exams:
+        table.append([
+            exam.name,
+            exam.description,
+            exam.exam_at,
+            exam.exam_type,
+            exam.location,
+            exam.url,
+            '<button type="button" class="btn btn-default">Edit</button> <button type="button" class="btn btn-danger" onclick="delete_row({})">Delete</button>'.format(exam.id)
+        ])
+
+    result = {'status':'success', 'message':message, 'data':{'table':table}}
+    return HttpResponse(json.dumps(result))
+
+@require_http_methods(["GET"])
+def teacher_index(request):
+    """
+    Display teacher list page
+    """
+    teachers = Teacher.objects.all().filter(deleted_at__isnull=True)
+
+    content_dict = {}
+    content_dict['teachers'] = teachers
+    content_dict['current_nav_id'] = 'nav-teacher'
+    return render(request, 'teacher/index.html', content_dict)
+
+@require_http_methods(["POST"])
+def teacher_post(request):
+
+    # get data from inputs
+    user_name = request.POST.get('user_name', None)
+    email = request.POST.get('email', None)
+
+    # check if data illegal
+    if user_name is None or user_name == '':
+        result = {'status':'error', 'message':'Illegal name!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    if email is None or email == '':
+        result = {'status':'error', 'message':'Illegal email!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+
+    # create new teacher
+    teacher = Teacher.objects.create(user_name=user_name, email=email)
+
+    # transfer teachers objects to table data
+    teachers = Teacher.objects.all().filter(deleted_at__isnull=True)
+    table = []
+    for teacher in teachers:
+        table.append([
+            teacher.user_name,
+            teacher.email,
+            '<button type="button" class="btn btn-default">Edit</button> <button type="button" class="btn btn-danger" onclick="delete_row({})">Delete</button>'.format(teacher.id)
+        ])
+
+    result = {'status':'success', 'message':'Create success!', 'data':{'table':table}}
+    return HttpResponse(json.dumps(result))
+
+@require_http_methods(["DELETE", "PUT"])
+def teacher_delete_or_update(request, teacher_id):
+    # get object
+    teacher = Teacher.objects.get(id=teacher_id, deleted_at__isnull=True)
+
+    # check if data illegal
+    if teacher is None:
+        result = {'status':'error', 'message':'This teacher not exists!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    
+    if request.method == 'DELETE':
+        # delete it
+        teacher.delete()
+        message = 'Delete success!'
+    elif request.method == 'PUT':
+        from django.http import QueryDict
+        put = QueryDict(request.body)
+
+        # get data from inputs
+        user_name = put.get('user_name', None)
+        email = put.get('email', None)
+
+        # check if data illegal
+        if user_name is None or user_name == '':
+            result = {'status':'error', 'message':'Illegal name!', 'data':{}}
+            return HttpResponse(json.dumps(result))
+        if email is None or email == '':
+            result = {'status':'error', 'message':'Illegal email!', 'data':{}}
+            return HttpResponse(json.dumps(result))
+        
+        # update data
+        teacher.user_name = user_name
+        teacher.email = email
+        teacher.save()
+
+        message = 'Update success!'
+
+    teachers = Teacher.objects.all().filter(deleted_at__isnull=True)
+    table = []
+    for teacher in teachers:
+        table.append([
+            teacher.name,
+            teacher.email,
+            '<button type="button" class="btn btn-default">Edit</button> <button type="button" class="btn btn-danger" onclick="delete_row({})">Delete</button>'.format(teacher.id)
+        ])
+
+    result = {'status':'success', 'message':message, 'data':{'table':table}}
+    return HttpResponse(json.dumps(result))
+
+@require_http_methods(["GET"])
+def question_index(request):
+    """
+    Display question list page
+    """
+    questions = Question.objects.all().filter(deleted_at__isnull=True)
+
+    content_dict = {}
+    content_dict['questions'] = questions
+    content_dict['current_nav_id'] = 'nav-question'
+    return render(request, 'question/index.html', content_dict)
+
+@require_http_methods(["DELETE"])
+def question_delete_or_update(request, question_id):
+    # get object
+    question = Question.objects.get(id=question_id, deleted_at__isnull=True)
+
+    # check if data illegal
+    if question is None:
+        result = {'status':'error', 'message':'This question not exists!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    
+    if request.method == 'DELETE':
+        # delete it
+        question.delete()
+        message = 'Delete success!'
+
+    questions = Question.objects.all().filter(deleted_at__isnull=True)
+    table = []
+    for question in questions:
+        table.append([
+            question.user_id, 
+            question.user_name,
+            question.question_content,
+            '<button type="button" class="btn btn-danger" onclick="delete_row({})">Delete</button>'.format(question.id)
+        ])
+
+    result = {'status':'success', 'message':message, 'data':{'table':table}}
+    return HttpResponse(json.dumps(result))
+
+@require_http_methods(["GET"])
+def query_history_index(request):
+    """
+    Display query_history list page
+    """
+    query_history_list = QueryHistory.objects.all().filter(deleted_at__isnull=True)
+
+    content_dict = {}
+    content_dict['query_history_list'] = query_history_list
+    content_dict['current_nav_id'] = 'nav-query_history'
+    return render(request, 'query_history/index.html', content_dict)
+
+@require_http_methods(["DELETE"])
+def query_history_delete_or_update(request, query_history_id):
+    # get object
+    query_history = QueryHistory.objects.get(id=query_history_id, deleted_at__isnull=True)
+
+    # check if data illegal
+    if query_history is None:
+        result = {'status':'error', 'message':'This query_history not exists!', 'data':{}}
+        return HttpResponse(json.dumps(result))
+    
+    if request.method == 'DELETE':
+        # delete it
+        query_history.delete()
+        message = 'Delete success!'
+
+    query_history_list = QueryHistory.objects.all().filter(deleted_at__isnull=True)
+    table = []
+    for query_history in query_history_list:
+        table.append([
+            query_history.user_id, 
+            query_history.query_content,
+            '<button type="button" class="btn btn-default">Edit</button> <button type="button" class="btn btn-danger" onclick="delete_row({})">Delete</button>'.format(query_history.id)
+        ])
+
+    result = {'status':'success', 'message':message, 'data':{'table':table}}
+    return HttpResponse(json.dumps(result))
